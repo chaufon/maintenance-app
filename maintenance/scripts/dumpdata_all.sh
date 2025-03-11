dda906cdbdd3794a9a24edb0f7fb2ca5229416b7
Carlos Laurente <ogti096@minsa.gob.pe>
2025-03-11 12:06:51 -0500

    Remove scripts and added initial migration.

--- maintenance/scripts/dumpdata_all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-python manage.py dumpdata --indent 2 users.Rol > apps/users/fixtures/roles.json
-python manage.py dumpdata --indent 2 users.User > apps/users/fixtures/users.json
-python manage.py dumpdata --indent 2 common.Departamento > apps/common/fixtures/departamentos.json
-python manage.py dumpdata --indent 2 common.Provincia > apps/common/fixtures/provincias.json
-python manage.py dumpdata --indent 2 common.Distrito > apps/common/fixtures/distritos.json
--- maintenance/scripts/just_rebuild.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-python manage.py reset_db
-python manage.py migrate
-python manage.py loaddata roles.json
-python manage.py loaddata departamentos.json
-python manage.py loaddata provincias.json
-python manage.py loaddata distritos.json
-python manage.py loaddata test_users.json
