f73a3642e1cbe4b0ded6513298b78e4f8543593f
Carlos Laurente <ogti096@minsa.gob.pe>
2025-10-27 12:36:31 -0500

    Dependencies updated.

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from setuptools import find_packages, setup
 
 setup(
     name="maintenance-app",
-    version="1.0.0",
+    version="1.1.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        "Django>=5.2.1",
-        "psycopg>=3.2.9",
-        "psycopg-c>=3.2.9",
-        "django-pghistory>=3.6.0",
-        "django-template-partials>=24.4",
-        "sentry-sdk[django]>=2.29.1",
-        "tablib[xlsx]>=3.8.0",
+        "Django>=5.2.7",
+        "psycopg>=3.2.12",
+        "psycopg-c>=3.2.12",
+        "django-pghistory>=3.8.3",
+        "django-template-partials>=25.2",
+        "sentry-sdk[django]>=2.42.1",
+        "tablib[xlsx]>=3.9.0",
         "python-magic>=0.4.27",
     ],
 )
